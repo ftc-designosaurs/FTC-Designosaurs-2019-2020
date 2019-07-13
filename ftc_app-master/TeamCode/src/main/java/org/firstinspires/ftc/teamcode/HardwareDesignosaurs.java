521b345ed10b8affa9d5d033b5ee10879d59a0f5
Speedy6451 <37423245+Speedy6451@users.noreply.github.com>
2019-07-13 09:10:23 -0500

    initial commit

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareDesignosaurs.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class HardwareDesignosaurs {
+    
+    // Define Motors
+    public DcMotor frontRight = null;
+    public DcMotor frontLeft  = null;
+    public DcMotor backRight  = null;
+    public DcMotor backLeft   = null;
+    
+    // Define Servos
+    
+    // Define Sensors
+    
+    // Define Variables
+    
+    HardwareMap hwMap = null;
+    
+    public HardwareDesignosaurs() {
+        
+    }
+    
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+
+        // Initialize Motors
+        frontRight = hwMap.get(DcMotor.class,"front_right");
+        frontLeft = hwMap.get(DcMotor.class,"front_left");
+        backRight = hwMap.get(DcMotor.class,"back_right");
+        backLeft = hwMap.get(DcMotor.class,"back_left");
+
+        // Initialize Servos
+
+        // Initialize Sensors
+
+        // Set Motor Directions
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        // Stop Motors
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backRight.setPower(0);
+        backLeft.setPower(0);
+
+        // Enable All Encoders
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    // Functions
+}
---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StandardTeleOp.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Mecanum Drive", group="TeleOp")
+public class StandardTeleOp extends OpMode {
+
+    HardwareDesignosaurs Robot = new HardwareDesignosaurs();
+
+    @Override
+    public void init() {
+        Robot.init(hardwareMap); //initialize motors and sensors
+
+        telemetry.addData("status: ", "Ready!");
+    }
+
+    @Override
+    public void loop() {
+        double fr, fl, bl, br; // Motor variables
+        double lh, lv, rh;     // Joystick variables
+
+        // set joystick variables
+        lh = gamepad1.left_stick_x;
+        lv = gamepad1.left_stick_y;
+        rh = gamepad1.right_stick_x;
+
+        // calculate motor powers for mecanum drive
+        fl = lv + lh + rh;
+        fr = lv - lh - rh;
+        bl = lv - lh + rh;
+        br = lv + lh - rh;
+
+        // set motor speeds
+        Robot.frontRight.setPower(fr);
+        Robot.frontLeft.setPower(fl);
+        Robot.backRight.setPower(br);
+        Robot.backLeft.setPower(bl);
+    }
+
+    @Override
+    public void stop() {
+        telemetry.addData("status: ","Done!");
+    }
+
+}

42151424858948be7361c327d94d2805c5644b12
Speedy6451 <37423245+Speedy6451@users.noreply.github.com>
2019-12-14 14:42:35 -0600

    imu debugging

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareDesignosaurs.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareDesignosaurs.java
@@ -42,10 +42,10 @@ public class HardwareDesignosaurs {
     public Servo capstoneGripper    = null;
 
     // Define Sensors
-    public BNO055IMU imu = null;
-    Orientation angles = null;
-    Acceleration gravity;
-    double heading = 0;
+//    public BNO055IMU imu = null;
+//    Orientation angles = null;
+//    Acceleration gravity;
+//    double heading = 0;
 
     // Define Variables
     public double startEncoder;
@@ -96,7 +96,7 @@ public class HardwareDesignosaurs {
 
         // Initialize Sensors
 
-        imu = hwMap.get(BNO055IMU.class,"imu");
+//        imu = hwMap.get(BNO055IMU.class,"imu");
 
 
         // Set Motor Directions
@@ -132,26 +132,26 @@ public class HardwareDesignosaurs {
     }
 
     // Functions
-    public void initIMU() {
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
-        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-
-        imu.initialize(parameters);
-
-        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
-
-    }
-
-
-    public void updateIMU() {
-        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-        heading = AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle));
-    }
+//    public void initIMU() {
+//        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+//        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+//        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+//        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+//        parameters.loggingEnabled      = true;
+//        parameters.loggingTag          = "IMU";
+//        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+//
+//        imu.initialize(parameters);
+//
+//        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+//
+//    }
+//
+//
+//    public void updateIMU() {
+//        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+//        heading = AngleUnit.DEGREES.normalize(AngleUnit.DEGREES.fromUnit(angles.angleUnit, angles.firstAngle));
+//    }
 
 
     public double square(double base, int power) { //function that does fractional squaring
--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imuTest.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imuTest.java
@@ -1,66 +1,66 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
-
-@Autonomous(name = "IMU Test OpMode", group = "IMU")
-public class imuTest extends LinearOpMode {
-
-    HardwareDesignosaurs Robot = new HardwareDesignosaurs();
-    private ElapsedTime runtime = new ElapsedTime();
-
-    double speed = .2;
-    double skewGain = 0.001;
-
-    @Override
-    public void runOpMode() {
-        telemetry.addData("init: ", "robot");
-        telemetry.update();
-        Robot.init(hardwareMap, 0, 0, 0);
-        telemetry.addData("init: ", "gyro");
-        telemetry.update();
-        Robot.initIMU();
-        telemetry.addData("init: ", "done");
-        telemetry.update();
-        waitForStart();
-
-        runtime.reset();
-        Robot.updateIMU();
-        double target = Robot.heading;
-        while (opModeIsActive()) {
-            Robot.updateIMU();
-            double skewComp = (target - Robot.heading) * skewGain;
-            Robot.frontLeft.setPower(-(speed) + skewComp);
-            Robot.frontRight.setPower(-(speed) - skewComp);
-            Robot.backLeft.setPower(speed + skewComp);
-            Robot.backRight.setPower(speed - skewComp);
-
-            telemetry.addData("Heading: ", Robot.angles.firstAngle);
-            telemetry.addData("Normal Heading: ", Robot.heading);
-            telemetry.addData("Roll: ", Robot.angles.secondAngle);
-            telemetry.addData("Pitch: ", Robot.angles.thirdAngle);
-            telemetry.update();
-        }
-        Robot.setPowers(Robot,0);
-
-    }
-
-    String formatAngle(AngleUnit angleUnit, double angle) {
-        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
-    }
-
-    String formatDegrees(double degrees){
-        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
-    }
-
-}
-
-
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//
+//import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+//import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+//import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+//
+//import java.util.Locale;
+//import java.util.concurrent.TimeUnit;
+//
+//@Autonomous(name = "IMU Test OpMode", group = "IMU")
+//public class imuTest extends LinearOpMode {
+//
+//    HardwareDesignosaurs Robot = new HardwareDesignosaurs();
+//    private ElapsedTime runtime = new ElapsedTime();
+//
+//    double speed = .2;
+//    double skewGain = 0.001;
+//
+//    @Override
+//    public void runOpMode() {
+//        telemetry.addData("init: ", "robot");
+//        telemetry.update();
+//        Robot.init(hardwareMap, 0, 0, 0);
+//        telemetry.addData("init: ", "gyro");
+//        telemetry.update();
+//        Robot.initIMU();
+//        telemetry.addData("init: ", "done");
+//        telemetry.update();
+//        waitForStart();
+//
+//        runtime.reset();
+//        Robot.updateIMU();
+//        double target = Robot.heading;
+//        while (opModeIsActive()) {
+//            Robot.updateIMU();
+//            double skewComp = (target - Robot.heading) * skewGain;
+//            Robot.frontLeft.setPower(-(speed) + skewComp);
+//            Robot.frontRight.setPower(-(speed) - skewComp);
+//            Robot.backLeft.setPower(speed + skewComp);
+//            Robot.backRight.setPower(speed - skewComp);
+//
+//            telemetry.addData("Heading: ", Robot.angles.firstAngle);
+//            telemetry.addData("Normal Heading: ", Robot.heading);
+//            telemetry.addData("Roll: ", Robot.angles.secondAngle);
+//            telemetry.addData("Pitch: ", Robot.angles.thirdAngle);
+//            telemetry.update();
+//        }
+//        Robot.setPowers(Robot,0);
+//
+//    }
+//
+//    String formatAngle(AngleUnit angleUnit, double angle) {
+//        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+//    }
+//
+//    String formatDegrees(double degrees){
+//        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+//    }
+//
+//}
+//
+//

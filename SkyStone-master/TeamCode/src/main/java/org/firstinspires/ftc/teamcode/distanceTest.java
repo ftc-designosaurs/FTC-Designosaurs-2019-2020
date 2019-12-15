6ed92783f8893fd3182d6f6cf7ae804daca52c85
Speedy6451 <37423245+Speedy6451@users.noreply.github.com>
2019-12-14 19:31:55 -0600

    omg the coding works!

---
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/distanceTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp(name = "Sensor: REV2mDistance p test", group = "Sensor")
+
+public class distanceTest extends LinearOpMode {
+
+    public DistanceSensor distance;
+    HardwareDesignosaurs Robot = new HardwareDesignosaurs();
+    public double speed = 0;
+    public double nominalSpeed = 0.2;
+    public double speedGain = 0.1;
+
+    @Override
+    public void runOpMode() {
+        distance = hardwareMap.get(DistanceSensor.class,"sensor_range");
+        Robot.init(hardwareMap,0,0,0);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            speed = (20 - distance.getDistance(DistanceUnit.INCH)) * speedGain;
+            Robot.frontLeft.setPower(-(speed));
+            Robot.frontRight.setPower(-(speed));
+            Robot.backLeft.setPower(speed);
+            Robot.backRight.setPower(speed);
+        }
+
+        Robot.setPowers(Robot,0);
+    }
+}
